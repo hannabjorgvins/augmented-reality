a9e3a7c40b8bf710dba8145b465cce2335c22ad7
Robin Seibold <robinbobseibold@gmail.com>
2016-05-03 21:10:03 +0200

    Add class handling the 3D rendering with SceneKit

--- AugmentedReality/AugmentedReality/ModelView.swift
+++ AugmentedReality/AugmentedReality/ModelView.swift
@@ -12,7 +12,7 @@ class ModelView : SCNScene {
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        let boxGeometry = SCNBox(width: 5.0, height: 5.0, length: 5.0, chamferRadius: 0.0)
+        let boxGeometry = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0.0)
         objectNode.addChildNode(SCNNode(geometry: boxGeometry))
         objectNode.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(2, y: 2, z: 2, duration: 1)))
         
