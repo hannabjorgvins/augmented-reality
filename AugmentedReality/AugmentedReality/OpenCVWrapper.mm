db34350a6beea3ba14b066d7fb7bc0e64eb7a913
Robin Seibold <robinbobseibold@gmail.com>
2016-05-12 21:17:53 +0200

    Add correct openCV -> openGL transformation matrix

--- AugmentedReality/AugmentedReality/OpenCVWrapper.mm
+++ AugmentedReality/AugmentedReality/OpenCVWrapper.mm
@@ -6,15 +6,15 @@
 #include <opencv2/imgcodecs/ios.h>
 
 #include <stdio.h>
+#include <math.h>
 
 @implementation OpenCVWrapper : NSObject
 
 cv::Mat cameraMat;
 cv::Mat distortionCoeffs;
-cv::Mat objPoints;
+std::vector<cv::Point3f> objPoints;
 
 + (NSArray*)getTransformationMatrixBetweenObjectPointsAndImage: (UIImage*) image {
-    std::cout << cameraMat << std::endl;
     cv::Mat imgWithAlpha;
     UIImageToMat(image, imgWithAlpha);
     
@@ -29,22 +29,22 @@ cv::Mat objPoints;
     
     if (!markerCorners.empty()) {
         cv::Mat viewMatrix = solvePnPForImagePoints(markerCorners[0]);
-        return @[[NSNumber numberWithFloat: viewMatrix.at<float>(0, 0)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(1, 0)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(2, 0)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(3, 0)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(0, 1)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(1, 1)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(2, 1)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(3, 1)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(0, 2)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(1, 2)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(2, 2)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(3, 2)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(0, 3)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(1, 3)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(2, 3)],
-                 [NSNumber numberWithFloat: viewMatrix.at<float>(3, 3)]];
+        return @[[NSNumber numberWithFloat: viewMatrix.at<double>(0, 0)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(1, 0)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(2, 0)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(3, 0)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(0, 1)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(1, 1)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(2, 1)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(3, 1)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(0, 2)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(1, 2)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(2, 2)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(3, 2)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(0, 3)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(1, 3)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(2, 3)],
+                 [NSNumber numberWithFloat: viewMatrix.at<double>(3, 3)]];
     }
     
     return @[];
@@ -59,14 +59,25 @@ cv::Mat objPoints;
 }
 
 + (void)setObjectPoints: (NSArray*) objectPoints {
-    objPoints = createMatrixFromArray(objectPoints, 3, 4);
+    objPoints.push_back(cv::Point3f([[objectPoints objectAtIndex: 0] doubleValue],
+                                    [[objectPoints objectAtIndex: 4] doubleValue],
+                                    [[objectPoints objectAtIndex: 8] doubleValue]));
+    objPoints.push_back(cv::Point3f([[objectPoints objectAtIndex: 1] doubleValue],
+                                    [[objectPoints objectAtIndex: 5] doubleValue],
+                                    [[objectPoints objectAtIndex: 9] doubleValue]));
+    objPoints.push_back(cv::Point3f([[objectPoints objectAtIndex: 2] doubleValue],
+                                    [[objectPoints objectAtIndex: 6] doubleValue],
+                                    [[objectPoints objectAtIndex: 10] doubleValue]));
+    objPoints.push_back(cv::Point3f([[objectPoints objectAtIndex: 3] doubleValue],
+                                    [[objectPoints objectAtIndex: 7] doubleValue],
+                                    [[objectPoints objectAtIndex: 11] doubleValue]));
 }
 
 cv::Mat createMatrixFromArray(NSArray* array, int m, int n) {
-    cv::Mat matrix = cv::Mat::zeros(m, n, CV_32F);
+    cv::Mat matrix = cv::Mat::zeros(m, n, CV_64F);
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            matrix.at<float>(i, j) = [[array objectAtIndex: (i * n) + j] floatValue];
+            matrix.at<double>(i, j) = [[array objectAtIndex: (i * n) + j] doubleValue];
         }
     }
     return matrix;
@@ -76,26 +87,29 @@ cv::Mat solvePnPForImagePoints(std::vector<cv::Point2f> imagePoints) {
     cv::Mat rvec, tvec;
     cv::solvePnP(objPoints, imagePoints, cameraMat, distortionCoeffs, rvec, tvec);
     
-    cv::Mat rotationMatrix = cv::Mat::zeros(4, 4, CV_32F);
+    cv::Mat rotationMatrix;
     cv::Rodrigues(rvec, rotationMatrix);
     
-    cv::Mat viewMatrix = cv::Mat::zeros(4, 4, CV_32F);
-    viewMatrix.at<float>(3, 3) = 1.0;
+    rotationMatrix = rotationMatrix.t();
+    tvec = -rotationMatrix * tvec;
+    
+    cv::Mat transformationMatrix = cv::Mat::zeros(4, 4, CV_64F);
+    transformationMatrix.at<double>(3, 3) = 1.0;
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            viewMatrix.at<float>(i, j) = rotationMatrix.at<float>(i, j);
+            transformationMatrix.at<double>(i, j) = rotationMatrix.at<double>(i, j);
         }
-        viewMatrix.at<float>(i, 3) = tvec.at<float>(i, 0);
+        transformationMatrix.at<double>(i, 3) = tvec.at<double>(i, 0);
     }
     
-    cv::Mat axisInversion = cv::Mat::zeros(4, 4, CV_32F);
-    axisInversion.at<float>(0, 0) =  1.0f;
-    axisInversion.at<float>(1, 1) = -1.0f;
-    axisInversion.at<float>(2, 2) = -1.0f;
-    axisInversion.at<float>(3, 3) =  1.0f;
-    viewMatrix = axisInversion * viewMatrix;
+    cv::Mat axisInversion = cv::Mat::zeros(4, 4, CV_64F);
+    axisInversion.at<double>(0, 0) =  1.0f;
+    axisInversion.at<double>(1, 1) = -1.0f;
+    axisInversion.at<double>(2, 2) = -1.0f;
+    axisInversion.at<double>(3, 3) =  1.0f;
+    transformationMatrix = transformationMatrix * axisInversion;
     
-    return viewMatrix;
+    return transformationMatrix;
 }
 
 @end
